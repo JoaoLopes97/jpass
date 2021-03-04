031bbf7c75aa6d9737427bba61246233ad0c0c7a
JoaoLopes97 <joaodiogolopes97@hotmail.com>
2021-03-04 16:33:58 +0000

    test of method DateUtils.createFormatter

--- src/test/java/jpass/util/DateUtilsTest.java
+++ src/test/java/jpass/util/DateUtilsTest.java
@@ -1,4 +1,59 @@
 package jpass.util;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class DateUtilsTest {
+
+
+    @Test
+    public void shouldFormatValidFormatTest(){
+
+        String validFormat = "dd.MM.yyyy";
+        String expectedFormatter = "Value(DayOfMonth,2)'.'Value(MonthOfYear,2)'.'Value(YearOfEra,4,19,EXCEEDS_PAD)";
+
+        DateTimeFormatter formatter = DateUtils.createFormatter(validFormat);
+
+        assertEquals(expectedFormatter,formatter.toString());
+    }
+
+   @Test
+   public void shouldFormatInvalidFormatTest(){
+
+       String invalidFormat = "yyy mmm dddd";
+       String expectedFormatter = "ParseCaseSensitive(false)(Value(Year,4,10,EXCEEDS_PAD)'-'Value(MonthOfYear,2)'-'Value(DayOfMonth,2))[Offset(+HH:MM:ss,'Z')]";
+
+       DateTimeFormatter formatter = DateUtils.createFormatter(invalidFormat);
+
+       assertEquals(expectedFormatter,formatter.toString());
+   }
+
+   @Test
+    public void shouldFormatNullTest(){
+
+       String expectedFormatter = "ParseCaseSensitive(false)(Value(Year,4,10,EXCEEDS_PAD)'-'Value(MonthOfYear,2)'-'Value(DayOfMonth,2))[Offset(+HH:MM:ss,'Z')]";
+
+       DateTimeFormatter formatter = DateUtils.createFormatter(null);
+
+       assertEquals(expectedFormatter,formatter.toString());
+   }
+
+   @Test
+    public void shouldFormatEmptyFormatTest(){
+
+        String emptyFormat = "";
+
+
+       String expectedFormatter = "ParseCaseSensitive(false)(Value(Year,4,10,EXCEEDS_PAD)'-'Value(MonthOfYear,2)'-'Value(DayOfMonth,2))[Offset(+HH:MM:ss,'Z')]";
+
+       DateTimeFormatter formatter = DateUtils.createFormatter(emptyFormat);
+
+       assertEquals(expectedFormatter,formatter.toString());
+   }
 }
