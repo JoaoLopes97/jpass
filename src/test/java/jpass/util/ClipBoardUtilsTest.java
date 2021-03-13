e7bb64fc4b2398ea4ce8f6a295c9a6bdbab78136
JoaoLopes97 <joaodiogolopes97@hotmail.com>
2021-03-13 10:33:54 +0000

    ClipboardUtils boundary value analysis

--- src/test/java/jpass/util/ClipBoardUtilsTest.java
+++ src/test/java/jpass/util/ClipBoardUtilsTest.java
@@ -43,6 +43,78 @@ public class ClipBoardUtilsTest {
 
 	//region ASSIGNMENT 2
 
+	@Test
+	public void ClipBoardContentStringLength0Test() throws Exception {
+
+		String stringContent = "";
+
+		ClipboardUtils.setClipboardContent(stringContent);
+
+		assertEquals(null, ClipboardUtils.getClipboardContent());
+	}
+
+	@Test
+	public void ClipBoardContentStringLength1Test() throws Exception {
+
+		String stringContent = "T";
+		String expectedResult = "T";
+
+		ClipboardUtils.setClipboardContent(stringContent);
+
+		assertEquals(expectedResult, ClipboardUtils.getClipboardContent());
+	}
+
+	@Test
+	public void ClipBoardContentStringWithCharsBelow32Test() throws Exception {
+
+		char invalidChar = 0x19; // Decimal value 31
+		String stringContent = Character.toString(invalidChar);
+		stringContent += "1234";
+		String expectedResult = stringContent;
+
+		ClipboardUtils.setClipboardContent(stringContent);
+
+		assertEquals(expectedResult, ClipboardUtils.getClipboardContent());
+	}
+
+	@Test
+	public void ClipBoardContentStringWithCharsEqual32Test() throws Exception {
+
+		char validChar = 0x20; // Decimal value 32
+		String stringContent = Character.toString(validChar);
+		stringContent += "1234";
+		String expectedResult = " 1234";
+
+		ClipboardUtils.setClipboardContent(stringContent);
+
+		assertEquals(expectedResult, ClipboardUtils.getClipboardContent());
+	}
+
+	@Test
+	public void ClipBoardContentStringWithCharsEqual127Test() throws Exception {
+
+		char invalidChar = 0x7f; // Decimal value 31
+		String stringContent = Character.toString(invalidChar);
+		stringContent += "1234";
+		String expectedResult = stringContent;
+
+		ClipboardUtils.setClipboardContent(stringContent);
+
+		assertEquals(expectedResult, ClipboardUtils.getClipboardContent());
+	}
+
+	@Test
+	public void ClipBoardContentStringWithCharsHigher127Test() throws Exception {
+
+		char validChar = 0x80; // Decimal value 32
+		String stringContent = Character.toString(validChar);
+		stringContent += "1234";
+		String expectedResult = stringContent;
+
+		ClipboardUtils.setClipboardContent(stringContent);
+
+		assertEquals(expectedResult, ClipboardUtils.getClipboardContent());
+	}
 	//endregion
 
 }
